a6bd5570a2d9ee33062c767e4a9b645841ee2b75
Anna <anasoft@yandex.ru>
2022-01-03 17:03:44 +0500

    Initial commit

---
+++ app/src/main/java/ru/anasoft/kinopoisk/Film.kt
@@ -0,0 +1,5 @@
+package ru.anasoft.kinopoisk
+
+class Film {
+
+}
---
+++ app/src/main/java/ru/anasoft/kinopoisk/MainActivity.kt
@@ -0,0 +1,22 @@
+package ru.anasoft.kinopoisk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.anasoft.kinopoisk.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        if(savedInstanceState == null){
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commit()
+        }
+    }
+}
